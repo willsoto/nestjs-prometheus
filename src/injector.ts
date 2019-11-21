024834729c0e9dbfd1eb066cd7201d60a09f7540
Will Soto <willsoto@users.noreply.github.com>
2019-11-21 17:58:30 -0500

    style(injector): add return type
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- src/injector.ts
+++ src/injector.ts
@@ -1,7 +1,9 @@
 import { Inject } from "@nestjs/common";
 import { getToken } from "./metrics";
 
-export function InjectMetric(name: string) {
+export function InjectMetric(
+  name: string,
+): (target: object, key: string | symbol, index?: number | undefined) => void {
   const token = getToken(name);
 
   return Inject(token);

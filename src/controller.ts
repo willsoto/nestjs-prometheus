e4a022cde8e0e9aae84ec2be098442217f9f8849
Will Soto <willsoto@users.noreply.github.com>
2020-11-07 08:49:32 -0500

    fix(controller): don't include Express types
    Closes #530
    
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- src/controller.ts
+++ src/controller.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Controller, Get, Res } from "@nestjs/common";
-import { Response } from "express";
 import client from "prom-client";
 
 /**
@@ -10,8 +11,15 @@ import client from "prom-client";
 @Controller()
 export class PrometheusController {
   @Get()
-  index(@Res() response: Response): void {
+  index(@Res() response: unknown): void {
+    // See this issue for why we type this as `unknown`
+    // https://github.com/willsoto/nestjs-prometheus/issues/530
+    // I currently don't know of any type from NestJS that captures the
+    // union of all available responses and I don't want to force users to
+    // download types for a framework they aren't using.
+    // @ts-expect-error
     response.header("Content-Type", client.register.contentType);
+    // @ts-expect-error
     response.send(client.register.metrics());
   }
 }

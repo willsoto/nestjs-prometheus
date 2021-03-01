04eca8429cd50fc4204a2640aa695b5c2b5fa582
Will Soto <willsoto@users.noreply.github.com>
2021-03-01 18:20:21 -0500

    style(*): update eslint config
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
-    "prettier/@typescript-eslint",
   ],
   env: {
     node: true,

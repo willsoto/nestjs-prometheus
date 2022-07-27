d75a5bcdc9650eb726dd104bfc28319969ac8427
Will Soto <willsoto@users.noreply.github.com>
2022-07-26 20:01:46 -0400

    test(fastify): add fastify test with compression
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- test/fastify.spec.ts
+++ test/fastify.spec.ts
@@ -1,21 +1,16 @@
+import compression from "@fastify/compress";
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
 import { Test } from "@nestjs/testing";
 import { expect } from "chai";
+import { afterEach, beforeEach } from "mocha";
 import { register } from "prom-client";
 import { PrometheusModule } from "../src";
 
-describe("PrometheusModuleWithFastify", () => {
+describe("Fastify integration", () => {
   let app: NestFastifyApplication;
-
-  afterEach(async () => {
-    if (app) {
-      register.clear();
-      await app.close();
-    }
-  });
 
   beforeEach(async () => {
     const testingModule = await Test.createTestingModule({
@@ -25,16 +20,43 @@ describe("PrometheusModuleWithFastify", () => {
     app = testingModule.createNestApplication<NestFastifyApplication>(
       new FastifyAdapter(),
     );
-
-    await app.init();
   });
 
-  it("registers a /metrics endpoint", async () => {
-    const response = await app.inject({
-      method: "GET",
-      url: "/metrics",
+  afterEach(async () => {
+    register.clear();
+    await app.close();
+  });
+
+  describe("without compression", function () {
+    beforeEach(async () => {
+      await app.init();
     });
 
-    expect(response).to.have.property("statusCode").to.eql(200);
+    it("registers a /metrics endpoint", async () => {
+      const response = await app.inject({
+        method: "GET",
+        url: "/metrics",
+      });
+
+      expect(response).to.have.property("statusCode").to.eql(200);
+      expect(response.body).to.be.a("string");
+    });
+  });
+
+  describe("with compression", function () {
+    beforeEach(async () => {
+      await app.register(compression);
+      await app.init();
+    });
+
+    it("registers a /metrics endpoint", async () => {
+      const response = await app.inject({
+        method: "GET",
+        url: "/metrics",
+      });
+
+      expect(response).to.have.property("statusCode").to.eql(200);
+      expect(response.body).to.be.a("string");
+    });
   });
 });

f54acc8eff5a31f712abe6855d90da51e4f9d5e2
Will Soto <willsoto@users.noreply.github.com>
2023-06-17 08:42:09 -0400

    chore: remove explicit types from InjectMetric
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- src/injector.ts
+++ src/injector.ts
@@ -20,14 +20,6 @@ import { getToken } from "./metrics";
  * }
  * ```
  */
-export function InjectMetric(
-  name: string,
-): (
-  target: object,
-  key: string | symbol | undefined,
-  index?: number | undefined,
-) => void {
-  const token = getToken(name);
-
-  return Inject(token);
+export function InjectMetric(name: string) {
+  return Inject(getToken(name));
 }

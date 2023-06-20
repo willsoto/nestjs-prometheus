7bb694ce2ff95bd20327f98578b56c8bb55a810f
Ivan Kucher <ivankucher@Ivans-MacBook-Air.local>
2023-06-20 12:19:17 +0300

    fix: controller response type

--- src/controller.ts
+++ src/controller.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Controller, Get, Res } from "@nestjs/common";
+import { Response } from "express";
 import * as client from "prom-client";
 
 /**
@@ -11,14 +10,8 @@ import * as client from "prom-client";
 @Controller()
 export class PrometheusController {
   @Get()
-  async index(@Res({ passthrough: true }) response: unknown): Promise<string> {
-    // See this issue for why we type this as `unknown`
-    // https://github.com/willsoto/nestjs-prometheus/issues/530
-    // I currently don't know of any type from NestJS that captures the
-    // union of all available responses and I don't want to force users to
-    // download types for a framework they aren't using.
-    // @ts-expect-error
-    response.header("Content-Type", client.register.contentType);
+  async index(@Res({ passthrough: true }) response: Response): Promise<string> {
+    response.setHeader("Content-Type", client.register.contentType);
     return client.register.metrics();
   }
 }

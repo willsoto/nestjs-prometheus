55f969d85c995a29ba1b136281a10d48965e4db8
Will Soto <willsoto@users.noreply.github.com>
2023-09-23 15:53:51 -0400

    test: add e2e test case for #1900
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

---
+++ test/fixtures/core.module.ts
@@ -0,0 +1,42 @@
+import { Module } from "@nestjs/common";
+import {
+  PrometheusModule,
+  makeCounterProvider,
+  makeGaugeProvider,
+  makeHistogramProvider,
+  makeSummaryProvider,
+} from "../../src";
+
+const counter = makeCounterProvider({
+  name: "counter",
+  help: "counter helper",
+});
+
+const gauge = makeGaugeProvider({
+  name: "gauge",
+  help: "gauge helper",
+});
+
+@Module({
+  imports: [
+    PrometheusModule.register({
+      defaultMetrics: {
+        enabled: false,
+      },
+    }),
+  ],
+  providers: [
+    counter,
+    gauge,
+    makeHistogramProvider({
+      name: "histogram",
+      help: "histrogram helper",
+    }),
+    makeSummaryProvider({
+      name: "summary",
+      help: "summary helper",
+    }),
+  ],
+  exports: [PrometheusModule, counter, gauge],
+})
+export class CoreModule {}
